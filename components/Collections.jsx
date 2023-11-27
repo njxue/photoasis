52e6a3081326019c69f18305f93781abe05e2943
njxue <ngjxue@gmail.com>
2023-11-27 20:54:22 +0800

    Remove fetchCollections server action

--- components/Collections.jsx
+++ components/Collections.jsx
@@ -1,9 +1,20 @@
-import fetchCollections from "@actions/fetchCollections";
 import CollectionCard from "./CollectionCard";
+import { authOptions } from "@app/api/auth/[...nextauth]/route";
+import { PrismaClient } from "@prisma/client";
 
+async function fetchCollections() {
+  try {
+    const prisma = new PrismaClient();
+    return {
+      image: "https://cloudflare-b2.ngjxue.workers.dev/1/DSC_0814.jpg",
+      status: 200,
+    };
+  } catch (err) {
+    return { status: 500 };
+  }
+}
 const Collections = async ({}) => {
   const data = await fetchCollections();
-  console.log(data);
   return (
     <div className="flex flex-row gap-2 flex-wrap ">
       <CollectionCard data={data} />
