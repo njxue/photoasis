cec1de378aeff1b7624f9bda43cbb1f4f60f6b71
njxue <ngjxue@gmail.com>
2023-12-02 20:20:04 +0800

    Allow closing of modal when clicking outside

--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -1,13 +1,31 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import { ModalProvider } from "./ModalContext";
 
-const Modal = ({ isOpen, setOpen, children }) => {
+const Modal = ({ isOpen, setOpen, closeOnClickOutside, children }) => {
+  const modalRef = useRef();
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (!modalRef.current?.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+
+    if (isOpen && closeOnClickOutside) {
+      document.addEventListener("click", handleClick);
+    }
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [isOpen, modalRef]);
   return (
     isOpen && (
       <div className="fixed inset-0 flex flex-col items-center justify-center z-50 bg-gray-700 bg-opacity-30">
         <ModalProvider setOpen={setOpen}>
-          <div className="bg-white w-[70%] h-[90%] flex flex-col justify-between p-3">
+          <div
+            ref={modalRef}
+            className="bg-white w-[70%] h-[90%] flex flex-col justify-between p-3">
             {children}
           </div>
         </ModalProvider>
