3ca5b5a174ad65fad8830a057383210ee259ba0d
njxue <ngjxue@gmail.com>
2023-12-11 21:13:21 +0800

    Replace thumbnail if photo in thumbnail is deleted

--- actions/deletePhoto.js
+++ actions/deletePhoto.js
@@ -11,18 +11,57 @@ const deletePhoto = async (photo, pathname) => {
   const session = await getServerSession(authOptions);
   const uid = session?.user.id;
 
-  const res = await prisma.photo.delete({
-    where: {
-      pid,
-    },
-  });
+  await prisma.$transaction(async (prisma) => {
+    const collection = await prisma.collection.findUniqueOrThrow({
+      where: {
+        cid_uid: { cid, uid },
+      },
+      include: {
+        thumbnail: true,
+        photos: true,
+      },
+    });
 
-  if (res.count < 1) {
-    return {
-      status: 500,
-      message: "Unable to delete photo",
-    };
-  }
+    const res = await prisma.photo.delete({
+      where: {
+        pid,
+      },
+    });
+    if (res.count < 1) {
+      return {
+        status: 500,
+        message: "Unable to delete photo",
+      };
+    }
+
+    if (!collection) {
+      return {
+        status: 500,
+        message: "Unable to delete photo",
+      };
+    }
+
+    if (collection.thumbnailPid === pid) {
+      const existingPhotos = collection.photos.filter(
+        (photo) => photo.pid !== pid
+      );
+
+      if (existingPhotos.length > 0) {
+        await prisma.collection.update({
+          where: {
+            cid_uid: { cid, uid },
+          },
+          data: {
+            thumbnail: {
+              connect: {
+                pid: existingPhotos[0].pid,
+              },
+            },
+          },
+        });
+      }
+    }
+  });
 
   try {
     const b2 = new BackBlazeB2({
@@ -36,6 +75,7 @@ const deletePhoto = async (photo, pathname) => {
       fileName: `${uid}/${cid}/${name}`,
     });
     revalidatePath(pathname);
+
     return { status: 204, message: "Success" };
   } catch (err) {
     return { status: 204, message: "File not found" };
