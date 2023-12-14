332adc872d1b78bd15f8858d49e1fdd3de20f0a9
njxue <ngjxue@gmail.com>
2023-12-14 21:26:47 +0800

    Change submit button styles

--- app/common/SubmitButton.jsx
+++ app/common/SubmitButton.jsx
@@ -5,7 +5,9 @@ import { useFormStatus } from "react-dom";
 const SubmitButton = ({ style, text }) => {
   const { pending } = useFormStatus();
   return (
-    <button className="bg-green-500 w-full text-white" type="submit">
+    <button
+      className="bg-green-500 w-full h-9 text-white px-2 rounded"
+      type="submit">
       {pending ? "Loading..." : text ?? "Submit"}
     </button>
   );
