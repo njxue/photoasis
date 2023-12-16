2f509cec00550c7aff65398a832bcd8170aab0ae
njxue <ngjxue@gmail.com>
2023-12-16 21:38:14 +0800

    Highlight current tab in nav bar

--- app/Nav.jsx
+++ app/Nav.jsx
@@ -1,37 +1,49 @@
 "use client";
 
-import { getProviders, signIn, signOut, useSession } from "next-auth/react";
+import { getProviders, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 const Nav = ({}) => {
   const { data: session } = useSession();
-  const [providers, setProviders] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const res = await getProviders();
-      setProviders(res);
-    })();
-  }, []);
+  const path = usePathname();
+  console.log(path);
 
   return (
     <div className="flex flex-row justify-between items-center w-full h-full bg-white p-2 shadow-gray-300 shadow-md md:flex-col md:border-b-black">
       <Link href="/">
         <img
           src="/assets/icons/home.svg"
-          className="hover:opacity-50"
+          className={`${
+            path === "/" && "filter invert hue-rotate-180 brightness-200"
+          } hover:opacity-50`}
           width={30}
         />
       </Link>
       <div className="flex flex-row gap-5 md:flex-col md:justify-between md:h-1/2">
         <div className="flex flex-row gap-3 md:flex-col">
           <Link href="/album/new">
-            <img src="/assets/icons/add-album.svg" width={30} />
+            <img
+              src="/assets/icons/add-album.svg"
+              className={`${
+                path === "/album/new" &&
+                "filter invert hue-rotate-180 brightness-200"
+              } hover:opacity-50`}
+              width={30}
+            />
           </Link>
           <Link href="/photos">
-            <img src="/assets/icons/gallery.svg" width={30} />
+            <img
+              src="/assets/icons/gallery.svg"
+              className={`${
+                path === "/photos" &&
+                "filter invert hue-rotate-180 brightness-200"
+              } hover:opacity-50`}
+              width={30}
+            />
           </Link>
         </div>
         <div className="flex flex-row justify-between items-center gap-3 md:flex-col">
