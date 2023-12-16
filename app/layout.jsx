dd978929f52b907c287df39ecf3e05fe8ab0b6ee
njxue <ngjxue@gmail.com>
2023-12-16 21:13:31 +0800

    Change layout styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,16 +16,16 @@ const Layout = async ({ children }) => {
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <Provider>
-          <main className="app flex flex-col md:flex-row">
+          <main className="app">
             {session?.user ? (
-              <>
+              <div className="flex flex-col h-screen md:flex-row">
                 <div className="h-12 w-screen md:w-12 md:h-screen fixed z-50">
                   <Nav />
                 </div>
-                <div className="grow mt-12 md:ml-12 md:mt-0 p-3 bg-gray-200 h-screen overflow-auto">
+                <div className="grow mt-12 md:ml-12 md:mt-0 p-3 bg-gray-200 overflow-auto">
                   {children}
                 </div>
-              </>
+              </div>
             ) : (
               <Login />
             )}
