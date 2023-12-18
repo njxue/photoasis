ab91b09be4261b0964b8600eb9a771d0d4e7753b
njxue <ngjxue@gmail.com>
2023-12-18 15:23:34 +0800

    Remove AlbumHeader component
    Header and Body need to co-locate in the same component since the header
    contains controll elements that affect the view of the body. Co-locating
    them prevents passing states as props which can be quite hard to
    maintain

--- app/album/[id]/components/AlbumContainer.jsx
+++ app/album/[id]/components/AlbumContainer.jsx
@@ -1,9 +1,19 @@
 "use client";
 import { useState } from "react";
 import PhotoCard from "../../../common/PhotoCard";
-import AlbumHeader from "./AlbumHeader";
+import DeleteAlbumForm from "./Menu/DeleteAlbumForm";
+import AlbumMenu from "./Menu/AlbumMenu";
+import UpdateAlbumForm from "./Menu/UpdateAlbumForm";
+import AddPhotosForm from "./Menu/AddPhotosForm";
+
 const AlbumContainer = ({ albumData }) => {
   const [minimalisticView, setMinimalisticView] = useState(false);
+  const [isSelecting, setIsSelecting] = useState(false);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [isAddingPhotos, setIsAddingPhotos] = useState(false);
+
   const { photos } = albumData;
 
   return (
@@ -16,7 +26,45 @@ const AlbumContainer = ({ albumData }) => {
         />
       </div>
 
-      <AlbumHeader minimalisticView={minimalisticView} albumData={albumData} />
+      {!minimalisticView && (
+        <div className="p-1">
+          <div className="flex flex-row items-center justify-between mb-3 mt-2 font-light">
+            {isEditing ? (
+              <UpdateAlbumForm
+                onCancel={() => setIsEditing(false)}
+                onSuccess={() => setIsEditing(false)}
+                albumData={albumData}
+              />
+            ) : (
+              <div className="flex flex-row justify-between gap-12 items-center grow max-w-[100%]">
+                <p className="text-ellipsis overflow-hidden text-2xl md:text-3xl">
+                  {albumData.name}
+                </p>
+                <div className="flex flex-row gap-2 min-w-[70px]">
+                  <img src="/assets/icons/select.svg" width={30} />
+                  <AlbumMenu
+                    setIsDeleting={setIsDeleting}
+                    setIsEditing={setIsEditing}
+                    setIsAddingPhotos={setIsAddingPhotos}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+
+          <hr className="mb-3" />
+          <DeleteAlbumForm
+            albumData={albumData}
+            isDeleting={isDeleting}
+            setIsDeleting={setIsDeleting}
+          />
+          <AddPhotosForm
+            albumData={albumData}
+            show={isAddingPhotos}
+            setShow={setIsAddingPhotos}
+          />
+        </div>
+      )}
 
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
         {photos.map((photo) => (
