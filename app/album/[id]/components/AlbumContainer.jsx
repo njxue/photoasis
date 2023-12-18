328758ff406bb4eb7598e2dc24fa77d72ce58aba
njxue <ngjxue@gmail.com>
2023-12-18 15:16:55 +0800

    Remove unused imports

--- app/album/[id]/components/AlbumContainer.jsx
+++ app/album/[id]/components/AlbumContainer.jsx
@@ -1,50 +1,10 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import PhotoCard from "../../../common/PhotoCard";
-import AddPhotos from "./AddPhotos";
-import updateAlbum from "@actions/updateAlbum";
-import deleteAlbum from "@actions/deleteAlbum";
-import Modal from "@app/common/Modal/Modal";
-import { ModalHeader } from "@app/common/Modal/ModalHeader";
-import { ModalBody } from "@app/common/Modal/ModalBody";
-import SubmitButton from "@app/common/SubmitButton";
 import AlbumHeader from "./AlbumHeader";
 const AlbumContainer = ({ albumData }) => {
   const [minimalisticView, setMinimalisticView] = useState(false);
-  const [showMenuItems, setShowMenuItems] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
-
-  const menuRef = useRef();
-  const { photos, aid } = albumData;
-
-  useEffect(() => {
-    const handleClick = (e) => {
-      if (!menuRef.current?.contains(e.target)) {
-        setShowMenuItems(false);
-      }
-    };
-    if (showMenuItems) {
-      document.addEventListener("click", handleClick);
-    }
-    return () => document.removeEventListener("click", handleClick);
-  }, [showMenuItems]);
-
-  async function handleUpdateAlbum(formdata) {
-    const albumName = formdata.get("albumName");
-    await updateAlbum({ aid, albumName });
-    setIsEditing(false);
-  }
-
-  async function handleDeleteAlbum(formdata) {
-    const confirmText = formdata.get("deleteConfirmation");
-    if (confirmText === "Delete") {
-      const res = await deleteAlbum(aid);
-      if (res.status === 204) {
-        console.log("Redirecting");
-      }
-    }
-  }
+  const { photos } = albumData;
 
   return (
     <div className="h-full p-1">
