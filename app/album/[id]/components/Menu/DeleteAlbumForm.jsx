e35cbace8c5f8633f9f2a1ba34cfac997b4d2be0
njxue <ngjxue@gmail.com>
2023-12-18 17:00:26 +0800

    Add reusable confirmation modal

--- app/album/[id]/components/Menu/DeleteAlbumForm.jsx
+++ app/album/[id]/components/Menu/DeleteAlbumForm.jsx
@@ -1,48 +1,27 @@
 "use client";
 
 import deleteAlbum from "@actions/deleteAlbum";
-import Modal from "@app/common/Modal/Modal";
-import { ModalHeader } from "@app/common/Modal/ModalHeader";
-import { ModalBody } from "@app/common/Modal/ModalBody";
-import SubmitButton from "@app/common/SubmitButton";
+import ConfirmationModal from "@app/common/ConfirmationModal";
 
-const DeleteAlbumForm = ({ albumData, isDeleting, setIsDeleting }) => {
+const DeleteAlbumForm = ({
+  albumData,
+  isDeletingAlbum,
+  setIsDeletingAlbum,
+}) => {
   const { aid } = albumData;
-  async function handleDeleteAlbum(formdata) {
-    const confirmText = formdata.get("deleteConfirmation");
-    if (confirmText === "Delete") {
-      const res = await deleteAlbum(aid);
-      if (res.status === 204) {
-        console.log("Redirecting");
-      }
+  async function handleDeleteAlbum() {
+    const res = await deleteAlbum(aid);
+    if (res.status === 204) {
     }
   }
 
   return (
-    <Modal isOpen={isDeleting} setOpen={setIsDeleting} size="sm">
-      <ModalHeader>Are you sure you want to delete this album?</ModalHeader>
-      <ModalBody>
-        <form className="w-full" action={handleDeleteAlbum}>
-          <label for="deleteConfirmation">Type "Delete" to confirm: </label>
-          <input
-            className="w-full border border-solid border-gray-600 rounded p-1"
-            type="text"
-            name="deleteConfirmation"
-          />
-          <div className="flex flex-row gap-1 w-full mt-1">
-            <SubmitButton text="Yes" />
-
-            <button
-              onClick={() => {
-                setIsDeleting(false);
-              }}
-              className="bg-red-700 rounded p-2 text-white w-full">
-              No
-            </button>
-          </div>
-        </form>
-      </ModalBody>
-    </Modal>
+    <ConfirmationModal
+      prompt="Are you sure you want to delete this album?"
+      isOpen={isDeletingAlbum}
+      setIsOpen={setIsDeletingAlbum}
+      onConfirm={handleDeleteAlbum}
+    />
   );
 };
 
---
+++ app/common/ConfirmationModal.jsx
@@ -0,0 +1,40 @@
+"use client";
+import Modal from "@app/common/Modal/Modal";
+import { ModalHeader } from "@app/common/Modal/ModalHeader";
+import { ModalBody } from "@app/common/Modal/ModalBody";
+import SubmitButton from "@app/common/SubmitButton";
+
+const ConfirmationModal = ({
+  isOpen,
+  setIsOpen,
+  onCancel,
+  onConfirm,
+  prompt,
+}) => {
+  return (
+    <Modal isOpen={isOpen} setOpen={setIsOpen} size="sm">
+      <ModalHeader>{prompt}</ModalHeader>
+      <ModalBody>
+        <form
+          className="w-full"
+          action={() => {
+            setIsOpen(false);
+            onConfirm && onConfirm();
+          }}>
+          <div className="flex flex-row gap-1 w-full mt-1">
+            <SubmitButton text="Yes" />
+            <button
+              onClick={() => {
+                setIsOpen(false);
+                onCancel && onCancel();
+              }}
+              className="bg-red-700 rounded text-white w-full">
+              No
+            </button>
+          </div>
+        </form>
+      </ModalBody>
+    </Modal>
+  );
+};
+export default ConfirmationModal;
