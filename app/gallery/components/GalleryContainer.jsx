15628dbd5f318456af5115720b44abe0c092c6f2
njxue <ngjxue@gmail.com>
2023-12-20 15:33:18 +0800

    Add minimalistic view to gallery page

---
+++ app/gallery/components/GalleryContainer.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import MinimalisticViewToggle from "@app/common/MinimalisticViewToggle";
+import PhotoCard from "@app/common/PhotoCard";
+import { useState } from "react";
+
+const GalleryContainer = ({ photos }) => {
+  const [minimalisticView, setMinimalisticView] = useState(false);
+  return (
+    <>
+      <div className="p-1">
+        {!minimalisticView && (
+          <div>
+            <div className="flex flex-row items-center gap-3 mb-3 mt-2 text-3xl font-light">
+              <p className="line-clamp-1">Gallery</p>
+            </div>
+            <hr className="mb-3" />
+          </div>
+        )}
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
+          {photos &&
+            photos.map((photo) => (
+              <PhotoCard
+                photo={photo}
+                expandable
+                minimalisticView={minimalisticView}
+              />
+            ))}
+        </div>
+      </div>
+      <MinimalisticViewToggle
+        minimalisticView={minimalisticView}
+        setMinimalisticView={setMinimalisticView}
+      />
+    </>
+  );
+};
+
+export default GalleryContainer;
--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -2,6 +2,7 @@ import { authOptions } from "@app/api/auth/[...nextauth]/route";
 import PhotoCard from "@app/common/PhotoCard";
 import prisma from "@prisma/prisma";
 import { getServerSession } from "next-auth";
+import GalleryContainer from "./components/GalleryContainer";
 const fetchPhotos = async (uid) => {
   let photos = await prisma.photo.findMany({
     where: {
@@ -20,19 +21,7 @@ const Page = async () => {
   const session = await getServerSession(authOptions);
   const uid = session?.user.id;
   const photos = await fetchPhotos(uid);
-  return (
-    <div className="p-1">
-      <div>
-        <div className="flex flex-row items-center gap-3 mb-3 mt-2 text-3xl font-light">
-          <p className="line-clamp-1">Gallery</p>
-        </div>
-        <hr className="mb-3" />
-      </div>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
-        {photos && photos.map((photo) => <PhotoCard photo={photo} expandable/>)}
-      </div>
-    </div>
-  );
+  return <GalleryContainer photos={photos} />;
 };
 
 export default Page;
