ca593258cb3e7eebb840b4b21df606d950cbf8be
njxue <ngjxue@gmail.com>
2023-12-26 15:20:07 +0800

    Reduce duplicated css styles

--- app/common/ImageUpload/PhotoSettingsInputs.jsx
+++ app/common/ImageUpload/PhotoSettingsInputs.jsx
@@ -1,13 +1,16 @@
 const PhotoSettingsInputs = ({ photo }) => {
+  const rowStyle = "flex flex-rows items-center justify-between gap-2";
+  const inputStyle =
+    "border border-solid border-gray-600 rounded p-1 grow md:max-w-[80%]";
   return (
     <div className="text-xs flex flex-col gap-1 px-1">
       <p className="mb-2 line text-sm text-start">{photo.name}</p>
-      <div className="flex flex-rows items-center justify-between gap-2">
+      <div className={rowStyle}>
         <label>
           <img src="/assets/icons/aperture-black.svg" width={20} />
         </label>
         <input
-          className="border border-solid border-gray-600 rounded p-1 grow md:max-w-[80%]"
+          className={inputStyle}
           type="number"
           name="aperture"
           step={0.1}
@@ -16,24 +19,24 @@ const PhotoSettingsInputs = ({ photo }) => {
           placeholder="Aperture"
         />
       </div>
-      <div className="flex flex-rows items-center justify-between gap-2">
+      <div className={rowStyle}>
         <label>
           <img src="/assets/icons/shutterspeed-black.svg" width={20} />
         </label>
         <input
-          className="border border-solid border-gray-600 rounded p-1 grow md:max-w-[80%]"
+          className={inputStyle}
           type="text"
           name="shutterspeed"
           defaultValue={photo.shutterspeed}
           placeholder="Shutter Speed"
         />
       </div>
-      <div className="flex flex-rows items-center justify-between gap-2">
+      <div className={rowStyle}>
         <label>
           <img src="/assets/icons/iso-black.svg" width={20} />
         </label>
         <input
-          className="border border-solid border-gray-600 rounded p-1 grow md:max-w-[80%]"
+          className={inputStyle}
           type="number"
           name="iso"
           min={0}
