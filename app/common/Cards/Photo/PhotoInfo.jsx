a1e452d0e777bd9dbf3bae8eaeeaf811e69df650
njxue <ngjxue@gmail.com>
2023-12-26 16:09:52 +0800

    Change styling of the photo info at the bottom of the photo cards

--- app/common/Cards/Photo/PhotoInfo.jsx
+++ app/common/Cards/Photo/PhotoInfo.jsx
@@ -1,18 +1,19 @@
 const PhotoInfo = ({ photo }) => {
+  const width = 15;
+  const itemStyle =
+    "flex flex-row justify-center items-center gap-2 flex-wrap basis-4/12";
   return (
-    <div className="flex flex-row justify-around items-center p-2 text-white">
-      <div className="flex flex-row items-center gap-2 flex-wrap">
-        <img src="/assets/icons/aperture-white.svg" width={20} />
+    <div className="flex flex-row justify-around items-center p-2 text-white text-xs">
+      <div className={itemStyle}>
+        <img src="/assets/icons/aperture-white.svg" width={width} />
         <div>{photo.aperture === "" ? "-" : photo.aperture}</div>
       </div>
-      <div className="border-r border-gray-200 h-[20px] w-[1px]"></div>
-      <div className="flex flex-row items-center gap-2 flex-wrap">
-        <img src="/assets/icons/shutterspeed-white.svg" width={20} />
+      <div className={itemStyle}>
+        <img src="/assets/icons/shutterspeed-white.svg" width={width} />
         <div>{photo.shutterspeed === "" ? "-" : photo.shutterspeed}</div>
       </div>
-      <div className="border-r border-gray-200 h-[20px] w-[1px]"></div>
-      <div className="flex flex-row items-center gap-2 flex-wrap justify-center">
-        <img src="/assets/icons/iso-white.svg" width={20} />
+      <div className={itemStyle}>
+        <img src="/assets/icons/iso-white.svg" width={width} />
         <div>{photo.iso === "" ? "-" : photo.iso}</div>
       </div>
     </div>
