510e7b36fff551e384aa488a269983038cc3cda6
njxue <ngjxue@gmail.com>
2024-01-06 14:35:28 +0800

    Set toast limit to 1

--- app/ToastContainerWrapper.jsx
+++ app/ToastContainerWrapper.jsx
@@ -4,6 +4,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const ToastContainerWrapper = () => {
-  return <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} />;
+  return <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} limit={1} />;
 };
 export default ToastContainerWrapper;
