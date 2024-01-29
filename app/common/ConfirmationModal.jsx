b18d65c166bb6082c5d502bf58a47cc37e363d2e
njxue <ngjxue@gmail.com>
2024-01-29 10:23:05 +0800

    Disable cancel button on confirm

--- app/common/ConfirmationModal.jsx
+++ app/common/ConfirmationModal.jsx
@@ -3,6 +3,7 @@ import Modal from "@app/common/Modal/Modal";
 import { ModalHeader } from "@app/common/Modal/ModalHeader";
 import { ModalBody } from "@app/common/Modal/ModalBody";
 import SubmitButton from "@app/common/SubmitButton";
+import { useState } from "react";
 
 const ConfirmationModal = ({
   isOpen,
@@ -11,24 +12,33 @@ const ConfirmationModal = ({
   onConfirm,
   prompt,
 }) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  function handleConfirm() {
+    setIsOpen(false);
+    onConfirm && onConfirm();
+  }
+
+  function handleCancel() {
+    setIsOpen(false);
+    onCancel && onCancel();
+  }
+
   return (
     <Modal isOpen={isOpen} setOpen={setIsOpen}>
       <ModalHeader>{prompt}</ModalHeader>
       <ModalBody>
         <form
           className="w-full"
-          action={() => {
-            setIsOpen(false);
-            onConfirm && onConfirm();
+          action={handleConfirm}
+          onSubmit={() => {
+            setIsSubmitting(true);
           }}>
           <div className="flex flex-row gap-1 w-full mt-1">
             <SubmitButton text="Yes" />
             <button
-              onClick={() => {
-                setIsOpen(false);
-                onCancel && onCancel();
-              }}
-              className="bg-red-700 rounded text-white w-full">
+              onClick={handleCancel}
+              disabled={isSubmitting}
+              className="bg-red-700 rounded text-white w-full disabled:opacity-50">
               No
             </button>
           </div>
