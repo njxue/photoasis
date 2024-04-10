9da4181ee121ac431ad2685edbda278d12de850b
njxue <ngjxue@gmail.com>
2024-04-10 22:39:28 +0800

    Re-issue refresh tokens when signing in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,6 +8,13 @@ const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
 
