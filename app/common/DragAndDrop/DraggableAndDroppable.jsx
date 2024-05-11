e0cb2998729fd8f0579e1ea46482e61f7045515f
njxue <ngjxue@gmail.com>
2024-05-11 14:47:54 +0800

    Add hover effect during drag and drop

--- app/common/DragAndDrop/DraggableAndDroppable.jsx
+++ app/common/DragAndDrop/DraggableAndDroppable.jsx
@@ -1,15 +1,23 @@
 "use client";
-
+import { useState } from "react";
 function DraggableAndDroppable({ children, handleDrag, handleDrop }) {
+  const [isDraggingOver, setIsDraggingOver] = useState(false);
   function handleDragOver(e) {
     e.preventDefault();
+    setIsDraggingOver(true);
   }
+
   return (
     <div
       draggable
       onDragStart={handleDrag}
-      onDrop={handleDrop}
-      onDragOver={handleDragOver}>
+      onDrop={(e) => {
+        handleDrop(e);
+        setIsDraggingOver(false);
+      }}
+      onDragOver={handleDragOver}
+      onDragLeave={() => setIsDraggingOver(false)}
+      className={`${isDraggingOver && "opacity-30"}`}>
       {children}
     </div>
   );
