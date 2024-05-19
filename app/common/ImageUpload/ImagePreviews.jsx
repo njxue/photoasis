5a46face31f9b5bd994e9f7dae1a344a4cb9b825
njxue <ngjxue@gmail.com>
2024-05-19 16:05:50 +0800

    Change styles

--- app/common/ImageUpload/ImagePreviews.jsx
+++ app/common/ImageUpload/ImagePreviews.jsx
@@ -14,7 +14,7 @@ const ImagePreviews = ({ images, setImages }) => {
   return (
     <div className="flex flex-col py-3 md:py-0 md:px-1 h-full">
       <div className="relative min-h-[90px] overflow-auto grow">
-        <div className="absolute top-0 left-0 w-full flex flex-row gap-1 md:grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="absolute top-0 left-0 w-full grid gap-1 grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {images &&
             images.map((image) => (
               <div className="relative min-w-[80px]" key={image.name}>
--- app/common/Select/SelectableItem.jsx
+++ app/common/Select/SelectableItem.jsx
@@ -12,8 +12,8 @@ const SelectableItem = ({ children, item }) => {
         {children}
       </div>
       {isSelecting && isSelected(item) && (
-        <div className="absolute top-0 right-0 p-1 opacity-60">
-          <img src="/assets/icons/tick-circle.svg" width={30} />
+        <div className="absolute top-0 right-0 p-1 bg-green-500 opacity-90">
+          <img src="/assets/icons/bold-tick.svg" width={16} />
         </div>
       )}
     </div>
