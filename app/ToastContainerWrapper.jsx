20802993cad4a3a0c677da74e57234c49c08a6d8
njxue <ngjxue@gmail.com>
2024-06-13 11:30:04 +0800

    Add toast configs to ToastWrapper

--- app/ToastContainerWrapper.jsx
+++ app/ToastContainerWrapper.jsx
@@ -4,6 +4,13 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const ToastContainerWrapper = () => {
-  return <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} limit={1} />;
+  return (
+    <ToastContainer
+      position={toast.POSITION.BOTTOM_RIGHT}
+      limit={1}
+      autoClose={3000}
+      closeButton={true}
+    />
+  );
 };
 export default ToastContainerWrapper;
