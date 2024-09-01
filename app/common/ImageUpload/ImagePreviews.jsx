13ef3ef9aa9d37b4cbce5745305089c2dfa491c5
njxue <ngjxue@gmail.com>
2024-09-01 11:53:03 +0800

    Hide remove button when form is submitting

--- app/common/ImageUpload/ImagePreviews.jsx
+++ app/common/ImageUpload/ImagePreviews.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import PhotoSettingsInputs from "./PhotoSettingsInputs";
 import OptimisedImage from "../Image/OptimisedImage";
 import { QUALITY_LOW } from "../Image/constants";
+import { useFormStatus } from "react-dom";
 
 const ImagePreviews = ({ images, setImages }) => {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const { pending } = useFormStatus();
 
   function handleRemoveImage(image) {
     const newImages = images.filter((i) => i.name !== image);
@@ -16,19 +18,19 @@ const ImagePreviews = ({ images, setImages }) => {
     <div className="flex flex-col py-3 md:py-0 md:px-1 h-full">
       <div className="relative min-h-[90px] overflow-auto grow">
         <div className="absolute top-0 left-0 w-full grid gap-1 grid-cols-3 xs:grid-cols-4 sm:grid-cols-6 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
-          {images &&
-            images.map((image) => (
-              <div className="relative min-w-[80px]" key={image.name}>
-                <OptimisedImage
-                  className={`${
-                    selectedPhoto === image.name && "opacity-30"
-                  } h-[80px] w-full`}
-                  src={image.url}
-                  onClick={(e) => setSelectedPhoto(e.target.id)}
-                  name={image.name}
-                  quality={QUALITY_LOW}
-                  hover
-                />
+          {images?.map((image) => (
+            <div className="relative min-w-[80px]" key={image.name}>
+              <OptimisedImage
+                className={`${
+                  selectedPhoto === image.name && "opacity-30"
+                } h-[80px] w-full`}
+                src={image.url}
+                onClick={(e) => setSelectedPhoto(e.target.id)}
+                name={image.name}
+                quality={QUALITY_LOW}
+                hover
+              />
+              {!pending && (
                 <div className="bg-black absolute right-0 top-0 cursor-pointer opacity-0 hover:opacity-70">
                   <img
                     src="/assets/icons/cross.svg"
@@ -42,8 +44,9 @@ const ImagePreviews = ({ images, setImages }) => {
                     className="invert"
                   />
                 </div>
-              </div>
-            ))}
+              )}
+            </div>
+          ))}
         </div>
       </div>
       <div className="min-h-[150px] mt-6" hidden={selectedPhoto == null}>
