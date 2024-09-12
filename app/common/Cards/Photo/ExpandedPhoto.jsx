36b04ccba53390bb0a4b27fc740a8d33e7a1fc70
njxue <ngjxue@gmail.com>
2024-09-12 23:30:01 +0800

    Fix image not being cached
    * There was a delay when clicking on an image in the album's page to
      expand it. This was because a fetch request was made for that image
    * The request was made because the image was not cached on initial page
      load (when all the photos in the album were fetched)
    * The unexpanded image and expanded image had different quality
      attributes, which generated different src attributes (quality is a
      query param). This resulted in a cache miss for the expanded image

--- app/common/Cards/Photo/ExpandedPhoto.jsx
+++ app/common/Cards/Photo/ExpandedPhoto.jsx
@@ -2,14 +2,14 @@ import Modal from "@app/common/Modal/Modal";
 import ExpandedPhotoInfo from "./ExpandedPhotoInfo";
 import OptimisedImage from "../../Image/OptimisedImage";
 import DownloadPhoto from "./DownloadPhoto";
-import { QUALITY_MAX } from "@app/common/Image/constants";
+import { QUALITY_MID } from "@app/common/Image/constants";
 const ExpandedPhoto = ({ expandPhoto, setExpandPhoto, photo }) => {
   return (
     <Modal isOpen={expandPhoto} setOpen={setExpandPhoto} closeOnClickOutside>
       <OptimisedImage
         src={photo.url}
         name={photo.name}
-        quality={QUALITY_MAX}
+        quality={QUALITY_MID}
         priority
       />
       <div className="absolute top-0 right-0">
