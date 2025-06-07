0e55e4018ceebce1d893ccb795842b16309397ce
njxue <ngjxue@gmail.com>
2025-06-07 10:33:57 +0800

    Increase minimum cache ttl for images to reduce transformations

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "cloudflare-b2.ngjxue.workers.dev"],
     unoptimized: false,
+    minimumCacheTTL: 2678400,
+    formats: ["image/webp"],
   },
 };
 
