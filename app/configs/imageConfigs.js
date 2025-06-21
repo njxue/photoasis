ee121df2cc25c403f6ba6d21ea277f6a769c3e06
njxue <ngjxue@gmail.com>
2025-06-21 22:54:57 +0800

    Fix feature flag

--- app/configs/imageConfigs.js
+++ app/configs/imageConfigs.js
@@ -7,4 +7,4 @@ export const IMAGE_SIZE_RESTRICTION_ENABLED =
   process.env.NEXT_PUBLIC_ENABLE_IMAGE_SIZE_RESTRICTION === "true";
 export const IMAGE_PLACEHOLDER = "/assets/images/placeholder.png";
 export const USE_NEXT_IMAGE =
-  process.env.NEXT_PUBLIC_ENABLE_NEXT_IMAGE_OPTIMIZATION === "false";
+  process.env.NEXT_PUBLIC_ENABLE_NEXT_IMAGE_OPTIMIZATION === "true";
