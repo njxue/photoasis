54441e1d57ad8e3ab36c40c4a42656079a0be352
njxue <ngjxue@gmail.com>
2025-07-01 20:58:00 +0800

    Use local env

--- app/configs/emailConfigs.js
+++ app/configs/emailConfigs.js
@@ -2,7 +2,4 @@ export const ADMIN_EMAIL_ADDRESS = process.env.ADMIN_EMAIL_ADDRESS;
 export const JWT_EMAIL_TOKEN_EXPIRY = "1d";
 export const JWT_EXPIRY_RESET_PASSWORD = "5m";
 export const RESEND_API_KEY = process.env.RESEND_API_KEY;
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? process.env.NEXT_PUBLIC_LOCAL_DOMAIN
-    : process.env.NEXT_PUBLIC_APP_DOMAIN;
+export const BASE_URL = process.env.NEXT_PUBLIC_APP_DOMAIN;
