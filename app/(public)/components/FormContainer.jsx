1bf303fa546ead766d20e5a6baea9ddf53be2acc
njxue <ngjxue@gmail.com>
2025-07-04 16:03:54 +0800

    Adjust form width

--- app/(public)/components/FormContainer.jsx
+++ app/(public)/components/FormContainer.jsx
@@ -1,6 +1,6 @@
 const FormContainer = ({ children }) => {
   return (
-    <div className="flex flex-col w-full xs:w-[400px] border rounded border-zinc-200 bg-white p-8 shadow-md">
+    <div className="flex flex-col w-[95%] xs:w-[400px] border rounded border-zinc-200 bg-white p-8 shadow-md">
       {children}
     </div>
   );
